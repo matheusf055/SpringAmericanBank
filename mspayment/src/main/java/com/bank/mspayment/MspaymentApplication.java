cb3f27291e6283d3c24efbc8fcfb8dbf8ba7dde7
Matheus Fernandes <m.cesar.nunes2014@gmail.com>
2024-06-27 19:32:29 -0300

    created the gateway to map the routes

--- mspayment/src/main/java/com/bank/mspayment/MspaymentApplication.java
+++ mspayment/src/main/java/com/bank/mspayment/MspaymentApplication.java
@@ -2,8 +2,10 @@ package com.bank.mspayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MspaymentApplication {
 
     public static void main(String[] args) {
